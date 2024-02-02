e4c7d4f1e9e48ad8976c6096a625b33ec0a1cace
bourichi taha <bourichi.taha@gmail.com>
2024-02-02 02:02:27 +0100

    created all the migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,10 @@ return new class extends Migration
     Schema::create('users', function (Blueprint $table) {
       $table->id();
       $table->string('email')->unique();
+      $table->string('username')->unique();
       $table->timestamp('email_verified_at')->nullable();
       $table->string('password');
+      $table->foreignId('image_id')->nullable()->constrained('uploads')->cascadeOnDelete();
       $table->rememberToken();
       $table->timestamps();
     });
---
+++ database/migrations/2024_02_02_005726_create_events_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+  public function up()
+  {
+      Schema::create('events', function (Blueprint $table) {
+          $table->id();
+          $table->string('name');
+          $table->text('description');
+          $table->integer('max_num_participants');
+          $table->date('date');
+          $table->time('start_time');
+          $table->time('end_time');
+          $table->foreignId('image_id')->nullable()->constrained('uploads')->cascadeOnDelete();
+          $table->foreignId('category_id')->constrained('categories')->cascadeOnDelete();
+          $table->foreignId('owner_id')->constrained('users')->cascadeOnDelete();
+          $table->foreignId('status_id')->constrained('statuses')->cascadeOnDelete();
+          $table->foreignId('location_id')->constrained('locations')->cascadeOnDelete();
+          $table->timestamps();
+      });
+  }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+  public function down()
+  {
+      Schema::dropIfExists('events');
+  }
+};
